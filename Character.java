dd2235b4b1382e5c48bc27062099ab6a553c11e4
xzhao <xzhao00@stuy.edu>
2019-02-26 21:39:22 -0500

    version1

---
+++ Character.java
@@ -0,0 +1,25 @@
+public class Character{
+    
+    protected int health;
+    protected int strength;
+    protected double attack;
+    protected int defense;
+    
+    public boolean isAlive(){
+        return health > 0;
+    }
+
+    private int getDefense(){
+        return defense;
+    }
+
+    private void lowerHP(int dmg){
+        health -= dmg;
+    }
+
+    public int attack(Character target){
+        int damage = (int)((strength * attack) - target.getDefense());
+        target.lowerHP(damage);
+        return damage;
+    }
+}
